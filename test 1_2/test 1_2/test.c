3609f0663c33888adf7a15a2abd33b823fb26cc6
LetZeroR <121796281+LetZeroR@users.noreply.github.com>
2023-01-03 21:32:47 +0800

    加法代码

---
+++ test 1_2/test 1_2/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	//printf("±»Ãÿ");
+//	int age =    19;
+//	double weight = 65.5;
+//	  
+//	age = age + 1;
+//	weight = weight - 5;
+//	printf("%d\n", age);
+//	printf("%If\n", weight);
+//
+//	return 0;
+//}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("sum=%d\n", sum);
+
+	return 0;
+}
